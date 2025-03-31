361e856462fb781dc7d141bbcb46ad6f1306ea82
Neelü <castorcanardes@gmail.com>
2025-03-31 16:59:05 +0200

    Add pique qui tue

---
+++ Assets/Script/CheckPoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public Transform respawnPoint;
+
+    SpriteRenderer spriteRenderer;
+    public Sprite passive, active;
+
+    private void Awake()
+    {
+
+    }
+
+}
