b4ece39a9cfe457493ac293f711c93ce9895a992
Neelü <castorcanardes@gmail.com>
2025-04-03 09:28:42 +0200

    add moray attack

---
+++ Assets/Script/MorayAttack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class MorayAttack : MonoBehaviour
+{
+    public float downSpeed = 1f;  
+    public float upSpeed = 2f;     
+    public float delayTime = 0.5f; 
+    public float maxDropDistance = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(AttackSequence());
+    }
+
+    private System.Collections.IEnumerator AttackSequence()
+    {
+        while (true) 
+        {
+            transform.DOMoveY(transform.position.y - maxDropDistance, downSpeed)
+                .SetEase(Ease.InQuad); 
+
+            yield return new WaitForSeconds(downSpeed);
+
+            transform.DOMoveY(transform.position.y + maxDropDistance, upSpeed)
+                .SetEase(Ease.OutQuad);
+
+            yield return new WaitForSeconds(upSpeed);
+
+            yield return new WaitForSeconds(delayTime);
+        }
+    }
+}
+
