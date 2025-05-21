ca8e185a55172dcbe95cf4259b84610d6e9f97c6
CastorCanard <castorcanardes@gmail.com>
2025-05-21 05:44:31 +0400

    moray start anims + cinematic working + jsp

--- Assets/Script/MorayAttackVerticaly.cs
+++ Assets/Script/MorayAttackVerticaly.cs
@@ -11,27 +11,43 @@ public class MorayAttackVerticaly : MonoBehaviour
     public float delayTime = 0.5f; 
     public float maxDropDistance = 5f;
 
+    [SerializeField] Animator animator;
+
+
+
     private void Start()
     {
         StartCoroutine(AttackSequence());
+
     }
 
     private System.Collections.IEnumerator AttackSequence()
     {
-        while (true) 
+        while (true)
         {
+            animator.SetFloat("Speedmultiplier", downSpeed / 0.2f);
+            animator.SetBool("isAttacking", true);
             transform.DOMoveY(transform.position.y - maxDropDistance, downSpeed)
-                .SetEase(Ease.InQuad); 
+                .SetEase(Ease.InQuad);
+
 
             yield return new WaitForSeconds(downSpeed);
+            animator.SetBool("isAttacking", false);
 
+            animator.SetFloat("Speedmultiplier", upSpeed / 0.7f);
+            animator.SetBool("isBacking", true);
             transform.DOMoveY(transform.position.y + maxDropDistance, upSpeed)
                 .SetEase(Ease.OutQuad);
 
             yield return new WaitForSeconds(upSpeed);
+            animator.SetBool("isBacking", false);
 
+            animator.SetBool("isWaiting", true);
+            animator.SetFloat("Speedmultiplier", delayTime / 0.08f);
             yield return new WaitForSeconds(delayTime);
+            animator.SetBool("isWaiting", false);
         }
+
     }
 }
 
